b43d0d5a45c7603b025a5765d57b3fec04b580db
Leonardo Machado Fabris <leonardomachado200112@gmail.com>
2023-04-25 14:26:07 -0300

    inside area header

---
+++ src/app/(inside)/pedidos/page.tsx
@@ -0,0 +1,14 @@
+"use client"
+
+import { Header } from "@/components/Header";
+import { Box } from "@mui/material";
+
+const Page = () => {
+    return (
+        <Box>
+            ...
+        </Box>
+    )
+}
+
+export default Page;
