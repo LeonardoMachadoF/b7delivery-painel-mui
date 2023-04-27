1e9f8a08f633f2a19b211081147af3133f253bff
Leonardo Machado Fabris <leonardomachado200112@gmail.com>
2023-04-27 02:32:35 -0300

    types and order item component on the products page

--- src/app/(inside)/pedidos/page.tsx
+++ src/app/(inside)/pedidos/page.tsx
@@ -1,13 +1,98 @@
 "use client"
 
-import { Header } from "@/components/Header";
-import { Box } from "@mui/material";
+import { OrderItem } from "@/components/OrderItem";
+import { api } from "@/libs/api";
+import { Order } from "@/types/Order";
+import { Refresh, Search } from "@mui/icons-material";
+import { Box, Button, CircularProgress, Grid, InputAdornment, Skeleton, TextField, Typography } from "@mui/material";
+import { useEffect, useState } from "react";
 
 const Page = () => {
+    const [searchInput, setSearchInput] = useState("");
+    const [loading, setLoading] = useState(true);
+    const [orders, setOrders] = useState<Order[]>([]);
+
+    const getOrders = async () => {
+        setSearchInput("");
+        setOrders([]);
+
+        setLoading(true);
+        const orderList: Order[] = await api.getOrders();
+        setOrders(orderList);
+        setLoading(false);
+    }
+
+    useEffect(() => {
+        getOrders();
+    }, [])
+
+    const handleSearchInput = () => { };
+
+    const handleSearchKey = () => { };
+
     return (
-        <Box>
-            ...
-        </Box>
+        <Box sx={{ my: 3 }}>
+            <Box sx={{ display: "flex", justifyContent: 'space-between', mb: 3 }}>
+                <Box sx={{ display: 'flex', alignItems: 'center' }}>
+                    <Typography component='h5' variant="h5" sx={{ color: '#555', mr: 2 }}>
+                        Pedidos
+                    </Typography>
+                    {loading ?
+                        <CircularProgress size={24} />
+                        :
+                        <Button onClick={getOrders} size="small" sx={{ justifyContent: { xs: 'flex-start', md: 'center' } }}>
+                            <Refresh />
+                            <Typography component="div" sx={{ color: '#555', display: { xs: 'none', sm: 'block' } }}>
+                                Atualizar
+                            </Typography>
+                        </Button>
+                    }
+
+                </Box>
+                <TextField
+                    value={searchInput}
+                    onChange={handleSearchInput}
+                    onKeyUp={handleSearchKey}
+                    placeholder="Pesquise um pedido"
+                    variant="standard"
+                    disabled={loading}
+                    InputProps={{
+                        endAdornment: (
+                            <InputAdornment position="end">
+                                <Search />
+                            </InputAdornment>
+                        )
+                    }}
+                />
+            </Box>
+
+            <Grid container spacing={3} columns={{ xs: 1, sm: 2, md: 4 }}>
+                {loading ?
+                    <>
+                        <Grid item xs={1}>
+                            <Skeleton variant="rectangular" height={220} />
+                        </Grid>
+                        <Grid item xs={1}>
+                            <Skeleton variant="rectangular" height={220} />
+                        </Grid>
+                        <Grid item xs={1}>
+                            <Skeleton variant="rectangular" height={220} />
+                        </Grid>
+                        <Grid item xs={1}>
+                            <Skeleton variant="rectangular" height={220} />
+                        </Grid>
+                    </>
+                    :
+                    orders.map((item, index) => {
+                        return (
+                            <Grid item xs={1}>
+                                <OrderItem item={item} />
+                            </Grid>
+                        )
+                    })
+                }
+            </Grid>
+        </Box >
     )
 }
 
---
+++ src/components/OrderItem.tsx
@@ -0,0 +1,12 @@
+import { Order } from "@/types/Order";
+import { Box } from "@mui/material";
+
+type Props = {
+    item: Order;
+}
+
+export const OrderItem = ({ item }: Props) => {
+    return (
+        <Box>...</Box>
+    );
+}
